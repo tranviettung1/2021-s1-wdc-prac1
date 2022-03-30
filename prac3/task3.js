1bb665c47b5b02292fc1d0eeb94764fb0a3d9dbd
tranviettung1 <89381412+tranviettung1@users.noreply.github.com>
2022-03-30 17:35:14 +0700

    Update task3.js

--- prac3/task3.js
+++ prac3/task3.js
@@ -10,31 +10,75 @@ function incMcount() {
   document.getElementById("mcount").innerText = num + 1;
 }
 
-function createMsg() {
-    const msg = document.getElementById("msg-input").value;
-    const post = document.getElementById("posts");
-  
-    const postTime = document.createElement("div");
-    postTime.innerText = new Date();
-    postTime.className = 'post-time';
+function createMessageNode({ content, color }) {
+  const postTime = document.createElement("div");
+  postTime.innerText = new Date();
+  postTime.className = "post-time";
 
-    const postContent = document.createElement("p");
-    postContent.innerText = msg;
-    postContent.className = 'post-content';
-  
-    post.appendChild(postTime);
-    post.appendChild(postContent);
+  const postContent = document.createElement("p");
+  postContent.innerText = content;
+  postContent.className = "post-content";
+
+  if (color) {
+    postContent.style.color = color;
+  }
+
+  const postWrap = document.createElement('div');
+  postWrap.appendChild(postTime);
+  postWrap.appendChild(postContent);
+  postWrap.className = 'post';
+  postWrap.id = 'msg-' + msgId;
+
+  msgId++;
+
+  return postWrap;
+}
+
+function createMsg() {
+  const msg = document.getElementById("msg-input").value;
+  const post = document.getElementById("posts");
+  const colors = document.getElementsByName('color');
+  const quantity = document.getElementById('quantity').value;
+
+  let color = null;
+  for (let i = 0; i< colors.length; i++) {
+    if(colors[i].checked) {
+      color = colors[i].value;
+      break;
+    }
+  }
+
+  if (quantity > 0) {
+    for (let i = 0; i < quantity; i++) {
+      const postNode = createMessageNode({content: msg, color});
+      post.appendChild(postNode);
+    }
+  }
 }
 
 function onMenuClick() {
-    const main = document.getElementById('main');
-    const menu = document.getElementById('menu');
+  const main = document.getElementById("main");
+  const menu = document.getElementById("menu");
 
-    if (main.classList.contains('hidden')) {
-        main.classList.remove('hidden');
-        return menu.classList.add('hidden');
-    }
+  if (main.classList.contains("hidden")) {
+    main.classList.remove("hidden");
+    return menu.classList.add("hidden");
+  }
 
-    main.classList.add('hidden');
-    return menu.classList.remove('hidden');
+  main.classList.add("hidden");
+  return menu.classList.remove("hidden");
 }
+
+function debounce(func, timeout = 300){
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => { func.apply(this, args); }, timeout);
+  };
+}
+function changeColor(){
+  const backgroundColor = document.getElementById('background-color-input').value;
+
+  document.body.style.backgroundColor = backgroundColor;
+}
+const processChange = debounce(() => changeColor(), 300);
