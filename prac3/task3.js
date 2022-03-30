d836cbde372e0c758bf322b90c3ed975491b58c1
tranviettung1 <89381412+tranviettung1@users.noreply.github.com>
2022-03-30 10:16:32 +0700

    Update task3.js

--- prac3/task3.js
+++ prac3/task3.js
@@ -12,19 +12,39 @@ function incMcount() {
 
 function createMsg() {
     const msg = document.getElementById("msg-input").value;
-    const parentMessage = document.getElementById("list-msg").value;
+    // const parentMessage = document.getElementById("list-msg").value;
     const post = document.getElementById("posts");
   
-    const msgTemplate = document.createElement("div");
-    const id = 'msg-' + msgId;
-    msgTemplate.id = id;
-    msgTemplate.innerHTML = `
-                <div class='post-time'>${new Date()}</div>
-                <p class='post-content'>${msg}</p>
-      `;
-    msgTemplate.className = parentMessage !== '-1' ? 'children-msg' : 'wrapper-message'
+    const postTime = document.createElement("div");
+    postTime.innerText = new Date();
+    postTime.className = 'post-time';
+
+    const postContent = document.createElement('p');
+    postContent.innerText = msg;
+    postContent.className = 'post-content';
+    // const id = 'msg-' + msgId;
+    // msgTemplate.id = id;
+    // msgTemplate.innerHTML = `
+    //             <div class='post-time'>${new Date()}</div>
+    //             <p class='post-content'>${msg}</p>
+    //   `;
+    // msgTemplate.className = parentMessage !== '-1' ? 'children-msg' : 'wrapper-message'
   
-    msgId++;
+    // msgId++;
   
-    post.appendChild(msgTemplate);
-  }
+    post.appendChild(postTime);
+    post.appendChild(postContent);
+}
+
+function onMenuClick() {
+    const main = document.getElementById('main');
+    const menu = document.getElementById('menu');
+
+    if (main.classList.contains('hidden')) {
+        main.classList.remove('hidden');
+        return menu.classList.add('hidden');
+    }
+
+    main.classList.add('hidden');
+    return menu.classList.remove('hidden');
+}
